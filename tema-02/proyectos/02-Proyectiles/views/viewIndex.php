45c9fe3b9c47a63931c331aa168b14f4082cc7f6
dlopnun1503 <dlopnun1503@g.educaand.es>
2024-10-02 12:58:34 +0200

    Proyecto Proyectiles empezado

---
+++ tema-02/proyectos/02-Proyectiles/views/viewIndex.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>2.2 Proyectiles</title>
+  <!-- css bootstrap 533 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <!-- bootstrap icon 1.11.3-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+  <div class="container">
+
+    <!-- cabecera documento -->
+    <header class="pb-3 mb-4 border-bottom">
+      <i class="bi bi-calculator"></i>
+      <span class="fs-6">Proyecto 2.2 - Proyectiles</span>
+    </header>
+
+    <!-- Formulario -->
+    <legend>Lanzamiento de Proyectiles</legend> <!-- legend permite poner un titulo -->
+    
+    <!-- Fin del formulario -->
+    <form method="POST">
+    
+
+      <!-- Valor 1 -->
+       <!-- 
+      En el input text cambiamos el tipo de dato que acepta 
+      el step sirve para indicar la cantidad de decimales que acepta
+      plceholder para que aparezca un valor por defecto
+       -->
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Velocida inicial</span>
+        <input type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" step="0.01" placeholder="0.00" name="Velocidad_Inicial">
+      </div>
+
+      <!-- Valor 2 -->
+       <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Angulo Lanzamiento</span>
+        <input type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" step="0.01" placeholder="0.00" name="Angulo_Lanzamiento">
+      </div>
+
+      <!-- Botones de acción -->
+       <div class="btn-group" role="group">
+        <button type="reset" class="btn btn-secondary">Borrar</button>
+        <button type="submit" class="btn btn-primary" formaction="calcular.php">Calcular</button>
+       </div>
+    </form>
+    
+
+    <!-- Pie del documento -->
+    <footer class="footer mt-auto py-3 fixed-bottom bg-ligth">
+      <div class="container">
+        <span class="text-muted">© 2024 David López Núñez - DWES - 2º DAW - Curso 24/25</span>
+      </div>
+    </footer>
+
+  </div>
+  <!-- javascript bootstrap 553 -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ tema-02/proyectos/02-Proyectiles/views/viewResultado.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>2.2 Proyectiles</title>
+    <!-- css bootstrap 533 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- bootstrap icon 1.11.3-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <!-- cabecera documento -->
+        <header class="pb-3 mb-4 border-bottom">
+            <i class="bi bi-calculator"></i>
+            <span class="fs-6">Proyecto 2.2 - Proyectiles</span>
+        </header>
+
+        <!-- Formulario -->
+        <legend>Lanzamiento de Proyectiles</legend> <!-- legend permite poner un titulo -->
+
+        <!-- Fin del formulario -->
+        <form></form>
+
+        <!-- Creamos una tabla -->
+        <table class="table table-hover">
+            <tbody>
+                <tr>
+                    <th scope="row">Valores Iniciales:</th>
+                </tr>
+                <tr>
+                    <td>Velocidad Inicial:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Ángulo de Inclinación:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">Resultados del lanzamiento:</th>
+                </tr>
+                <tr>
+                    <td>Ángulo Radianes:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Velocida Inicial X:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Velocida Inicial Y:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Alcance máximo del proyectil:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Tiempo de vuelo del proyectil:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Altura máxima del proyectil:</td>
+                    <td></td>
+                </tr>
+                
+              
+            </tbody>
+        </table>
+
+        <br>
+        <!-- Botones de acción -->
+        <div class="btn-group" role="group">
+            <a class="btn btn-primary" href="index.php" role="button">Volver</a>
+        </div>
+        </form>
+
+        <!-- Pie del documento -->
+        <footer class="footer mt-auto py-3 fixed-bottom bg-ligth">
+            <div class="container">
+                <span class="text-muted">© 2024 David López Núñez - DWES - 2º DAW - Curso 24/25</span>
+            </div>
+        </footer>
+
+    </div>
+    <!-- javascript bootstrap 553 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
