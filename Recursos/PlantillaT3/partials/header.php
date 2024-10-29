8e4a11a3460043234822480df3d8f22473715848
dlopnun1503 <dlopnun1503@g.educaand.es>
2024-10-29 11:05:27 +0100

    Examen Tema 3

--- Recursos/PlantillaT3/partials/header.php
+++
@@ -1,7 +0,0 @@
-<!-- Cabecera -->
-<header class="pb-3 mb-4 border-bottom">
-      <span class="fs-5">
-        <i class="bi bi-people-fill"></i>
-        Proyecto 31 - CRUD Alumnos Array <!-- Cambiar dependiendo del proyecto !!!!!!!!!!!!!!!! -->
-      </span>
-    </header>
