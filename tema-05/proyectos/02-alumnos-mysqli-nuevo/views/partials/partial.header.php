69f5781684c73c689b0f1d25b0df1bb76edb1af9
dlopnun1503 <dlopnun1503@g.educaand.es>
2024-11-28 10:49:32 +0100

    Proyecto alumnos terminado

--- tema-05/proyectos/02-alumnos-mysqli-nuevo/views/partials/partial.header.php
+++
@@ -1,7 +0,0 @@
-<!-- Cabecera del documento -->
-<header class="pb-3 mb-4 border-bottom">
-        <span class="fs-5">
-            <i class="bi bi-book-half"></i></i>
-            Proyecto 5.1 Gestión Alumnos 
-        </span>
-</header>
